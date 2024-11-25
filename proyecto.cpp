f7eb675c1c3f2e9b16e536df8092089f1ad2296e
sands722 <arenass186@gmail.com>
2024-11-24 18:09:25 -0600

    se verifica que funciona el repositorio

---
+++ proyecto.cpp
@@ -0,0 +1,23 @@
+/***************************************
+	UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO
+	FACULTAD DE ESTUDIOS SUPERIORES -ARAGON-
+
+	Computadoras y programacion.
+	(c) Ponga su nombre y numero de cuenta aqui.
+
+	Quiso decir: Programa principal de la aplicacion de la distancia de Levenstein.
+
+**************************************/
+
+
+#include "stdafx.h"
+#include <string.h>
+#include "corrector.h"
+//Funciones publicas del proyecto
+/***************************************
+	DICCIONARIO: Esta funcion crea el diccionario completo
+	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario
+	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
+	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
+	int &	iNumElementos			:	Numero de elementos en el diccionario
+**************************************/
