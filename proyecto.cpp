a035cb8a2dc1ea56904772186b28c5184a3a36f5
sands722 <arenass186@gmail.com>
2024-11-24 22:59:46 -0600

    funcion 3  primera version

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,7 +1,15 @@
 ClonaPalabras: toma una palabra y obtiene todas las combinaciones y permutaciones requeridas por el metodo
-char* szPalabraLeida,						// Palabra a clonar
-char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
-int& iNumSugeridas)						//Numero de elementos en la lista
-************************************** /
-// Función ClonaPalabraas
-
+	char *	szPalabraLeida,						// Palabra a clonar
+	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
+	int &	iNumSugeridas)						//Numero de elementos en la lista
+**************************************/
+// Función ClonaPalabras
+void ClonaPalabras(char* szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int& iNumSugeridas) {
+	iNumSugeridas = 0;
+	for (int i = 0; i < strlen(szPalabraLeida); i++) {
+		char palabraModificada[TAMTOKEN];
+		strcpy_s(palabraModificada, TAMTOKEN, szPalabraLeida);
+		memmove(&palabraModificada[i], &palabraModificada[i + 1], strlen(szPalabraLeida) - i);
+		strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, palabraModificada);
+	}
+}
