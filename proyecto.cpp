c15e3a54b250c9ba7ea242c063ebb7e4673b1616
sands722 <arenass186@gmail.com>
2024-11-24 22:58:42 -0600

    inicio funcion 3

--- proyecto.cpp
+++ proyecto.cpp
@@ -3,4 +3,5 @@ char* szPalabraLeida,						// Palabra a clonar
 char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
 int& iNumSugeridas)						//Numero de elementos en la lista
 ************************************** /
-// Función ClonaPalabras
+// Función ClonaPalabraas
+
