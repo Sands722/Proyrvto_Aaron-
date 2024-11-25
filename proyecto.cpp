3405c1a2fcb6b3dca7b7d787da935f11d9345a45
sands722 <arenass186@gmail.com>
2024-11-24 22:14:33 -0600

    primera version del codigo

--- proyecto.cpp
+++ proyecto.cpp
@@ -18,3 +18,35 @@ int iNumElementos,
 char szListaFinal[][TAMTOKEN],
 int iPeso[],
 int& iNumLista)
+{
+	iNumLista = 0; // Inicializamos el contador de la lista final
+
+	for (int i = 0; i < iNumSugeridas; i++) {
+		for (int j = 0; j < iNumElementos; j++) {
+			if (strcmp(szPalabrasSugeridas[i], szPalabras[j]) == 0) {
+				// Palabra encontrada en el diccionario
+				strcpy_s(szListaFinal[iNumLista], szPalabras[j]);
+				iPeso[iNumLista] = iEstadisticas[j];
+				iNumLista++;
+			}
+		}
+	}
+
+	// Ordenar szListaFinal por peso (burbuja por simplicidad)
+	for (int i = 0; i < iNumLista - 1; i++) {
+		for (int j = i + 1; j < iNumLista; j++) {
+			if (iPeso[i] < iPeso[j]) {
+				// Intercambiar palabras
+				char tempPalabra[TAMTOKEN];
+				strcpy_s(tempPalabra, szListaFinal[i]);
+				strcpy_s(szListaFinal[i], szListaFinal[j]);
+				strcpy_s(szListaFinal[j], tempPalabra);
+
+				// Intercambiar pesos
+				int tempPeso = iPeso[i];
+				iPeso[i] = iPeso[j];
+				iPeso[j] = tempPeso;
+			}
+		}
+	}
+}
